064c6960c193ae46c6f0c646d5c82bad81783f78
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2015-09-24 14:30:31 -0600

    Add normal handling

--- python/tonic/paraview/data_converter.py
+++ python/tonic/paraview/data_converter.py
@@ -1,6 +1,22 @@
 from vtk import *
 
+from tonic.camera import *
+
 import json, os, math, gzip, shutil
+
+# -----------------------------------------------------------------------------
+# Helper function
+# -----------------------------------------------------------------------------
+
+def getScalarFromRGB(rgb, scalarRange=[-1.0, 1.0]):
+    delta = (scalarRange[1] - scalarRange[0]) / 16777215.0 # 2^24 - 1 => 16,777,215
+    if rgb[0] != 0 or rgb[1] != 0 or rgb[2] != 0:
+        # Decode encoded value
+        return scalarRange[0] + delta * float(rgb[0]*65536 + rgb[1]*256 + rgb[2] - 1)
+    else:
+        # No value
+        return float('NaN')
+
 # -----------------------------------------------------------------------------
 # Composite.json To order.array
 # -----------------------------------------------------------------------------
@@ -123,9 +139,69 @@ class ConvertCompositeSpriteToSortedStack(object):
 
         # Encode Normals (sorted order way)
         if 'normal' in self.layers[0]:
-            # FIXME
-            # -> Need to extract (x,y,z) + camera => compute view normal
-            pass
+            sortedNormal = vtkUnsignedCharArray()
+            sortedNormal.SetNumberOfComponents(3) # x,y,z
+            sortedNormal.SetNumberOfTuples(stackSize)
+
+            # Get Camera orientation and rotation information
+            camDir = [0,0,0]
+            with open(os.path.join(directory, "camera.json"), "r") as f:
+                camera = json.load(f)
+                norm = 0
+                for i in range(3):
+                    camDir[i] = camera['position'][i] - camera['focalPoint'][i]
+                    norm += camDir[i] * camDir[i]
+                norm = math.sqrt(norm)
+                camDir = [ i / norm for i in camDir ]
+            rotAxis = [ camDir[1], -camDir[0], 0 ]
+            angle = math.acos(camDir[2])
+            # print "Camera orientation", camDir
+            # print "Rotation along", rotAxis, angle
+
+            # Tmp structure to capture (x,y,z) normal
+            normalByLayer = vtkFloatArray()
+            normalByLayer.SetNumberOfComponents(3)
+            normalByLayer.SetNumberOfTuples(stackSize)
+
+            # Capture all layer normals
+            layerIdx = 0
+            zPosCount = 0
+            zNegCount = 0
+            for layer in self.layers:
+                normalOffset = layer['normal']
+                for idx in range(imageSize):
+                    normalByLayer.SetTuple3(
+                        layerIdx * imageSize + idx,
+                        getScalarFromRGB(rgbArray.GetTuple(idx + normalOffset[0] * imageSize)),
+                        getScalarFromRGB(rgbArray.GetTuple(idx + normalOffset[1] * imageSize)),
+                        getScalarFromRGB(rgbArray.GetTuple(idx + normalOffset[2] * imageSize))
+                    )
+
+                    # Re-orient normal to be view based
+                    vect = normalByLayer.GetTuple3(layerIdx * imageSize + idx)
+                    if not math.isnan(vect[0]):
+                        rotation = axisangle_to_q(rotAxis, angle)
+                        rVect = qv_mult(rotation, vect)
+                        normalByLayer.SetTuple3(layerIdx * imageSize + idx, rVect[0], rVect[1], math.abs(rVect[2])) # Always toward camera
+
+                layerIdx += 1
+
+            # Sort normals and encode them as 3 bytes ( -1 < xy < 1 | 0 < z < 1)
+            for idx in range(stackSize):
+                layerIdx = int(orderArray.GetValue(idx))
+                if layerIdx == 255:
+                    # No normal => same as view direction
+                    sortedNormal.SetTuple3(idx, 128, 128, 255)
+                else:
+                    offset = layerIdx * imageSize
+                    imageIdx = idx % imageSize
+                    vect = normalByLayer.GetTuple3(imageIdx + offset)
+                    sortedNormal.SetTuple3(idx, int(127.5 * (vect[0] + 1)), int(127.5 * (vect[1] + 1)), int(255 * vect[0]))
+
+            # Write the sorted data
+            with open(os.path.join(directory, 'normal.uint8'), 'wb') as f:
+                f.write(buffer(sortedNormal))
+                self.data.append({'name': 'normal', 'type': 'array', 'fileName': '/normal.uint8', 'categories': ['normal']})
 
         # Encode Intensity (sorted order way)
         if 'intensity' in self.layers[0]:
---
+++ scripts/paraview/mpas/earth-contours-sorted-composite.py
@@ -0,0 +1,129 @@
+# -----------------------------------------------------------------------------
+# User configuration
+# -----------------------------------------------------------------------------
+
+outputDir = '/Volumes/KSW-Data/tonic/mpas-contours-sorted-composite/'
+inputFile = '/Volumes/KSW-Data/Data/DataExploration/Data/MPAS/data/xyz_n_primal/X_Y_Z_NLAYER-primal_%d_0.vtu'
+earthCore = '/Volumes/KSW-Data/Data/vtk/mpas/earth/earth-ok.vtk'
+
+phi   = range(0, 360, 30)
+theta = range(-60, 61, 30)
+time  = range(50, 5151, 50)
+
+dataRanges = {
+    'bottomDepth': [-9753, 5984],
+    'salinity': [24.8574, 37.4595],
+    'temperature': [-1.64296, 28.6918]
+}
+
+# -----------------------------------------------------------------------------
+
+from paraview import simple
+from tonic.paraview.dataset_builder import *
+
+# -----------------------------------------------------------------------------
+# Pipeline creation
+# -----------------------------------------------------------------------------
+
+core = simple.OpenDataFile(earthCore)
+coreSurface = simple.ExtractSurface(Input=core)
+coreWithNormals = simple.GenerateSurfaceNormals(Input=coreSurface)
+
+reader = simple.OpenDataFile(inputFile % time[0])
+reader.CellArrayStatus = ['temperature', 'salinity']
+
+dataCleanUp  = simple.Threshold(Input = reader, Scalars = ['CELLS', 'temperature'], ThresholdRange = [-1000.0, 50.0])
+dataToPoints = simple.CellDatatoPointData(Input = dataCleanUp)
+
+sceneDescription = {
+    'size': [500, 500],
+    'light': [ 'intensity', 'normal' ],
+    'camera': {
+        'CameraViewUp': [0.0, 0.0, 1.0],
+        'CameraPosition': [107823.5, -46073770.81765442, -44044.25],
+        'CameraFocalPoint': [107823.5, -7766.0, -44044.25]
+    },
+    'scene': [
+        {
+            'name': 'Earth',
+            'source': coreWithNormals,
+            'colors': {
+                'bottomDepth': {'location': 'POINT_DATA', 'range': dataRanges['bottomDepth'] }
+            }
+        },{
+            'parent': 'Temperatures',
+            'name': '5\ua8451',
+            'source': simple.Contour(
+                            Input = dataToPoints,
+                            PointMergeMethod = "Uniform Binning",
+                            ContourBy = 'temperature',
+                            Isosurfaces = [5.0]),
+            'colors': {
+                'temperature': {'constant': 5.0 },
+                'salinity': {'location': 'POINT_DATA', 'range': dataRanges['salinity'] }
+            }
+        },{
+            'parent': 'Temperatures',
+            'name': '10\ua8451',
+            'source': simple.Contour(
+                            Input = dataToPoints,
+                            PointMergeMethod = "Uniform Binning",
+                            ContourBy = 'temperature',
+                            Isosurfaces = [10.0]),
+            'colors': {
+                'temperature': {'constant': 10.0 },
+                'salinity': {'location': 'POINT_DATA', 'range': dataRanges['salinity'] }
+            }
+        },{
+            'parent': 'Temperatures',
+            'name': '15\ua8451',
+            'source': simple.Contour(
+                            Input = dataToPoints,
+                            PointMergeMethod = "Uniform Binning",
+                            ContourBy = 'temperature',
+                            Isosurfaces = [15.0]),
+            'colors': {
+                'temperature': {'constant': 15.0 },
+                'salinity': {'location': 'POINT_DATA', 'range': dataRanges['salinity'] }
+            }
+        },{
+            'parent': 'Temperatures',
+            'name': '20\ua8451',
+            'source': simple.Contour(
+                            Input = dataToPoints,
+                            PointMergeMethod = "Uniform Binning",
+                            ContourBy = 'temperature',
+                            Isosurfaces = [20.0]),
+            'colors': {
+                'temperature': {'constant': 20.0 },
+                'salinity': {'location': 'POINT_DATA', 'range': dataRanges['salinity'] }
+            }
+        },{
+            'parent': 'Temperatures',
+            'name': '25\ua8451',
+            'source': simple.Contour(
+                            Input = dataToPoints,
+                            PointMergeMethod = "Uniform Binning",
+                            ContourBy = 'temperature',
+                            Isosurfaces = [25.0]),
+            'colors': {
+                'temperature': {'constant': 25.0 },
+                'salinity': {'location': 'POINT_DATA', 'range': dataRanges['salinity'] }
+            }
+        }
+    ]
+}
+
+# -----------------------------------------------------------------------------
+# Data Generation
+# -----------------------------------------------------------------------------
+
+# Create Image Builder
+dsb = CompositeDataSetBuilder(outputDir, sceneDescription, {'type': 'spherical', 'phi': phi, 'theta': theta})
+
+dsb.start()
+for t in time:
+    reader.FileName = inputFile % t
+    dsb.writeData()
+dsb.stop()
+
